ad12f33e885ef08db907ceb27fd2c75a9a093591
Mitch24 <paulanthony1920@gmail.com>
2014-04-08 14:21:17 -0700

    adds comments to person.rb methods

--- ruby/person.rb
+++ ruby/person.rb
@@ -12,31 +12,31 @@ class Person
   def name
     attributes[:name]
   end
-
+  # This returns a string of the person's first name
   def first_name
     name.split(' ').first
   end
-
+  # This returns a string of the person's last name
   def last_name
     name.split(' ').last
   end
-
+  # This returns a string of the person's age
   def age
     attributes[:age]
   end
-
+  # This returns a fixnum of the person's birthyear
   def birthyear
      (age.to_i - Time.now.year.to_i)
   end
-
+  # This printss to the console
   def say(words)
     puts "#{words}"
   end
-
+  # This returns @energy minus a random subtracted from it
   def run
     @energy -= rand(5)
   end
-
+  # This returns a string describing the Person instances energy level
   def energy_level
     case @energy
     when -100..-1
@@ -49,7 +49,7 @@ class Person
       "ready to go"
     end
   end
-
+  # This is a setter to change the instances energy level
   def energy_level=(amount)
     @energy += amount
   end
